5428cc3c097ff0be6abbabfee6e3cd3569bd105f
Tiago Mazzon <tiago.mazzon@gmail.com>
2023-01-23 08:28:55 -0300

    feat: projeto do jogo Numero Secreto do curso JavaScript: validações e reconhecimento de voz

---
+++ numero_secreto/app/validacao.js
@@ -0,0 +1,56 @@
+function verificaSeOChutePossuiValorValido(chute) {
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
+    if (numeroDentroValoresPermitidos(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `;
+    }
+    else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `;
+    }
+    else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `;
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroDentroValoresPermitidos(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+});
